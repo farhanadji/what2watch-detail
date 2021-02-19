6b2c9d762299ea92ce27b73be2f5573fe4abd0ec
Farhan Adji <farhanadji@live.com>
2021-02-19 17:37:13 +0700

    minor

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         .package(name: "Realm", url: "https://github.com/realm/realm-cocoa.git", from: "10.5.1"),
         .package(url: "https://github.com/Alamofire/Alamofire.git", .upToNextMajor(from: "5.2.0")),
-        .package(url: "https://github.com/farhanadji/what2watch-core.git", .branch("master"))
+        .package(path: "../Core")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
